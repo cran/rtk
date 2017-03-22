5141f4ed124a0a1f1fe3df043462e6551cadf1d2
Paul Saary <rtk@paulsaary.de>
2017-03-22 21:39:11 +0000

    version 0.2.5

--- src/options.h
+++ src/options.h
@@ -1,29 +1,30 @@
 #pragma once
-#include "IO.h"
-
+//#include "IO.h"
 
 struct options
 {
 public:
 	options(int argc, char** argv);
+	options(std::string, std::string , int repeats, std::vector<long> depth, int NoOfMatrices, bool verbose, unsigned int threads);
 	void print_rare_details();
 	//~options();
 
 	//vars
-  string input = "";
-  string output = "";
-  string mode  = "";
-  string referenceDir = "";
-  string referenceFile = "";
-  string map = "";
-  double depth = 0.95;
-  uint repeats = 10;
-  uint write = 0;
-  uint threads = 1;
+  std::string input = "";
+  std::string output = "";
+  std::string mode  = "";
+  std::string referenceDir = "";
+  std::string referenceFile = "";
+  std::string map = "";
+  std::vector<long> depth;
+  long depthMin;
+  unsigned int repeats = 10;
+  unsigned int write = 0;
+  unsigned int threads = 1;
   bool writeSwap = true;
   bool verbose = false;
 
-  string modDB;
+  std::string modDB;
   int modRedund;
   float modEnzCompl;
   float modModCompl;
@@ -31,5 +32,5 @@ public:
   bool modCollapse;
   bool calcCoverage;
 
-  string xtra;
+  std::string xtra;
 };
